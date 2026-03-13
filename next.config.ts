92884e318f7b1a0c76ad570acc1967ba3289f948
nerokart <nkart@hotmail.com>
2026-03-14 00:11:33 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+// gemicha-v3 (Ana Site) / next.config.js
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/cosmos/:path*',
+        destination: 'https://gemicha-portal.vercel.app/cosmos/:path*', 
+        // Not: Senin o uzun linkin yerine projenin ana Vercel linkini yazdık.
+      },
+    ]
+  },
+}
