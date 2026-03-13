73224184dd74fa043e83b25df5d083c0de8159e8
nerokart <nkart@hotmail.com>
2026-03-14 00:21:06 +0300

    Delete src directory

--- src/lib/supabase.ts
+++
@@ -1,8 +0,0 @@
-// src/lib/supabase.ts
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-// Sunucu tarafında (SSR) verileri ışık hızında çekmek için tekil (singleton) istemcimiz
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
