eec93ab104de528bcba1b67ef01b172d378fb336
nerokart <nkart@hotmail.com>
2026-03-14 01:35:50 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
-  // SİHİRLİ DOKUNUŞ: Tarayıcıya CSS ve JS dosyalarını nereden alacağını kesin olarak söyler
+  // Görsellerin ve CSS'in ana sitede kırılmaması için:
   assetPrefix: 'https://gemicha-portal.vercel.app',
 };
 
